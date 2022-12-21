b86161507139a442712ff09191259b3125589b6d
AqibJavaid899 <aqibjaved0910@gmail.com>
2022-12-21 22:19:47 +0500

    Add a Docker Compose file with Mapped Volume and also implement a GET and POST routes awith JSON file working as a database

--- api/server.js
+++ api/server.js
@@ -1,17 +1,27 @@
 import express from "express";
 import cors from "cors";
-
-import data from "./data.js";
+import bodyParser from "body-parser";
+import fs from "fs";
 
 const app = express();
 
+let users = JSON.parse(fs.readFileSync("data.json").toString());
+
 // Handling the Permission regarding Resource Sharing
 app.use(cors());
+app.use(bodyParser.json());
 
 // GET Route which will return the JSON object
-app.use("/", (req, res) => {
-  res.status(200).json(data);
+app.get("/get", (req, res) => {
+  res.status(200).status(200).json(users);
 });
 
-const PORT = 4000;
-app.listen(PORT, () => console.log("Server is listening on PORT 4000"));
+app.post("/post", (req, res) => {
+  const user = req.body;
+  users = [...users, user];
+  fs.writeFileSync("data.json", JSON.stringify(users));
+  res.status(201).json(users);
+});
+
+const PORT = 4001;
+app.listen(PORT, () => console.log("Server is listening on PORT 4001"));
